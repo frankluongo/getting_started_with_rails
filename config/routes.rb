f5cf1a283b01d1b7657a8fdb190c4d1e6e2ce799
Frank Luongo <frankluongo92@gmail.com>
2018-08-27 11:33:43 -0400

    Completed up to Module 7

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # This tells the domain which page to point to...
   root 'welcome#index'
